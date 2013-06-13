991094f012eb8b80ccefa7127b77ee954d8aa848
Greg Kronmiller <greg_kronmiller@hmc.edu>
2013-06-13 04:12:00 -0400

    Commented the header files.

--- server/map_cell.hrl
+++ server/map_cell.hrl
@@ -1,7 +1,15 @@
+% Record for storing information about a single cell of a map.
 -record(map_cell, {
+    % blocks_passage: false if actors may freely walk through this square; true
+    % otherwise.
+    % FIXME: This should be determined by whether there is a wall on the cell.
     blocks_passage = false,
+    
+    % actors: a list of actors in the cell. The beginning of the list is the
+    % "top" of the pile of actors.
     actors = []
 }).
 
 % TODO: Have a record for the information that a particular actor knows about
 % a map cell?
+
