064477ad291480f623a8b0ef3898319d6d4f4a28
Greg Kronmiller <greg_kronmiller@hmc.edu>
2013-09-06 14:19:47 -0700

    Finished server IPC module.
    There's still a couple of kinks to work out, but it should be
    functional. Now we just need to actually use it.

--- server/inter_process.erl
+++ server/inter_process.erl
@@ -1,5 +1,3 @@
-% Note: this module is incomplete. Don't use it yet!
-
 -module(inter_process).
 
 -export([
@@ -9,12 +7,15 @@
     make_request/3,
     main_loop/2
 % For spawning
-    % wait_for_connections/0
+    % ...
 ]).
 
+% Amount of time after which to give up on waiting for a request, in
+% milliseconds.
+-define(REQUEST_TIMEOUT, 5000).
 
 
-% main_loop(Handler, Data):
+
 % This function should be spawned as a process.
 % 
 % Handler is a 2-tuple consisting of the name of a module and function as
@@ -30,33 +31,33 @@
 %       The Data object from above.
 %   * MessageType --
 %       An atom, either 'request' or 'notification'.
-%   * Sender --
-%       In the case of a request, the PID of the sender. In the case of a
-%       notification, the atom 'no_reply'.
 %   * MessageCommand --
 %       An atom indicating the command in the message.
 %   * Arguments --
 %       A tuple containing the arguments to the command.
 % 
-% The Handler function should return a 2-tuple {Stop, NewData}.
+% In the case of a notification, the Handler function should return a 2-tuple
+% {Stop, NewData}. In the case of a request, it should return a 3-tuple {Stop,
+% NewData, Response}.
 % Stop should be a boolean indicating whether this process should stop
 % executing. NewData should be the (possibly modified) value of Data to be
-% passed to the next iteration of the loop.
+% passed to the next iteration of the loop. Response (if applicable) should be
+% an arbitrary Erlang object to be sent back to the requester.
+% FIXME: What if there's an error? Should the third return be {ok, Response}?
 main_loop(Handler = {Module, Function}, Data) ->
     % Get the next message.
     receive
         {request, Sender, MessageCommand, Arguments} ->
             % Handle a request.
-            {Stop, NewData} = apply(Module, Function,
-                [Data, request, Sender, MessageCommand, Arguments])
-            % FIXME: Don't pass the sender to the Handler; instead return a
-            % 3-tuple in the case of requests and then send the response
-            % message to the caller here.
+            {Stop, NewData, Response} = apply(Module, Function,
+                [Data, request, MessageCommand, Arguments]),
+            % Send the response back to the requester.
+            send_response(Sender, Response, MessageCommand, Arguments)
     ;
         {notification, MessageCommand, Arguments} ->
             % Handle a notification.
             {Stop, NewData} = apply(Module, Function,
-                [Data, notification, no_reply, MessageCommand, Arguments])
+                [Data, notification, MessageCommand, Arguments])
     ;
         _ ->
             % Crash if we get a malformed message.
@@ -84,12 +85,14 @@ send_notification(Pid, MessageCommand, Arguments) ->
 
 
 % Makes the specified request of the process with the specified Pid.
+% MessageCommand should be an atom indicating the nature of the request;
+% Arguments should be a tuple of arguments clarifying the details of the
+% request.
 % Returns either {ok, Response} where Response is the response to that request,
 % or {error, Reason} if something went wrong.
 make_request(Pid, MessageCommand, Arguments) ->
     % Send the request.
     Pid ! {request, self(), MessageCommand, Arguments},
-    % TODO: Write the code that sends these messages.
     % FIXME: Add a single unifying atom to the beginning? Say, 'response'?
     receive
         {ok, MessageCommand, Response} ->
@@ -99,5 +102,14 @@ make_request(Pid, MessageCommand, Arguments) ->
         {error, Reason} ->
             % Recipient sent back an error.
             {error, Reason}
+    after
+        ?REQUEST_TIMEOUT ->
+            {error, timeout}
     end.
+
+% Send the given Response back to the Sender of a request.
+% MessageCommand and Arguments are the details of the request (see
+% make_request).
+send_response(Sender, Response, MessageCommand, _Arguments) ->
+    Sender ! {ok, MessageCommand, Response}.
 
